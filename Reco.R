46bbbe1593fc404feccd02bfb58e50f59e73e064
Vishnu Mohan <36357452+vishnu4mohan@users.noreply.github.com>
2018-04-26 10:18:03 +0530

    Update Reco.R

--- Reco.R
+++ Reco.R
@@ -19,13 +19,32 @@ if(!"h2o" %in% installed.packages()){
 	stop("Please install h2o package to continue.")
 }
 else{
-	try(h2o.init(nthreads=-1, max_mem_size = paste(ceiling(get_free_ram()/1024^2),"g", sep = "")), silent = TRUE)
-	if(h2o.clusterIsUp()){
-		cluster_status <- h2o.clusterStatus()
-		print(paste("H2O Cluster Total Memory", as.numeric(cluster_status$free_mem), sep = "-"))
-		print(paste("Number of CPUs in Use", as.numeric(cluster_status$num_cpus), sep = "-"))
-	}
-	else{
-		stop("Couldn't Connect to H2O Cluster.")
-	}
+		get_free_ram <- function(){
+		system <- Sys.info()[["sysname"]]
+		if(system == "Windows"){
+			x <- system2("wmic", args =  "OS get FreePhysicalMemory /Value", stdout = TRUE)
+			x <- x[grepl("FreePhysicalMemory", x)]
+			x <- gsub("FreePhysicalMemory=", "", x, fixed = TRUE)
+			x <- gsub("\r", "", x, fixed = TRUE)
+			as.integer(x)
+		} else if (system == "Linux"){
+			x <- system('grep MemFree /proc/meminfo', intern = TRUE)
+			x <- gsub("MemFree:        ", "",x, fixed = TRUE)
+			x <- gsub(" kB", "", x, fixed = TRUE)
+			as.integer(x)
+		}  
+		else {
+				stop("Only supported on Windows and Linux OS")
+			}
+		}
+		try(h2o.init(nthreads=-1, max_mem_size = paste(ceiling(get_free_ram()/1024^2),"g", sep = "")), silent = TRUE)
+		if(h2o.clusterIsUp()){
+			cluster_status <- h2o.clusterStatus()
+			print(paste("H2O Cluster Total Memory", as.numeric(cluster_status$free_mem), sep = "-"))
+			print(paste("Number of CPUs in Use", as.numeric(cluster_status$num_cpus), sep = "-"))
+			
+		}
+		else{
+			stop("Couldn't Connect to H2O Cluster.")
+		}
 }
